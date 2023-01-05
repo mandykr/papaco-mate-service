081ed1cfdd4ebf2315be8fa1acd470ca0ccc249e
mandykr <hongseokii92@gmail.com>
2023-01-05 15:53:05 +0900

    Initial commit

---
+++ src/main/java/com/papaco/papacomateservice/PapacoMateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.papaco.papacomateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapacoMateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapacoMateServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/papaco/papacomateservice/PapacoMateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaco.papacomateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapacoMateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
